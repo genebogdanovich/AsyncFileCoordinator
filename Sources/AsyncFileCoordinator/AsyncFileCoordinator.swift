452d2157ffc5044d09f33fea2c9710e776be8aa1
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-07-20 18:24:15 +0300

    Implement and test writing.

--- Sources/AsyncFileCoordinator/AsyncFileCoordinator.swift
+++ Sources/AsyncFileCoordinator/AsyncFileCoordinator.swift
@@ -1,2 +1,92 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+extension NSFileCoordinator {
+    // func coordinate(writingItemAt url: URL, options: NSFileCoordinator.WritingOptions = [], error outError: NSErrorPointer, byAccessor writer: (URL) -> Void) {
+    func coordinate(writing data: Data, at url: URL) async throws {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            
+            func handleWriting(newURL: URL) {
+                do {
+                    try data.write(to: newURL, options: .atomic)
+                    continuation.resume()
+                    return
+                } catch {
+                    continuation.resume(throwing: error)
+                    return
+                }
+            }
+            
+            var nsError: NSError?
+            
+            self.coordinate(writingItemAt: url, options: .forReplacing, error: &nsError, byAccessor: handleWriting)
+            if let nsError = nsError {
+                continuation.resume(throwing: nsError)
+                return
+            }
+        }
+    }
+    
+    /*
+    
+    
+    
+    // func coordinate(writingItemAt url: URL, options: NSFileCoordinator.WritingOptions = [], error outError: NSErrorPointer, byAccessor writer: (URL) -> Void)
+    
+    func coordinate(deletingItemAt url: URL) async throws {
+        // fileExists is light so use it to avoid coordinate if the file doesn’t exist.
+        guard FileManager.default.fileExists(atPath: url.path) else { return }
+        
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+            var nsError: NSError?
+            self.coordinate(
+                writingItemAt: url, options: .forDeleting, error: &nsError, byAccessor: { (newURL: URL) -> Void in
+                    do {
+                        try FileManager.default.removeItem(atPath: newURL.path)
+                        continuation.resume()
+                        return
+                    } catch {
+                        continuation.resume(throwing: error)
+                        return
+                    }
+                }
+            )
+            if let nsError = nsError {
+                continuation.resume(throwing: nsError)
+                return
+            }
+        }
+    }
+    
+    // func coordinate(readingItemAt url: URL, options: NSFileCoordinator.ReadingOptions = [], error outError: NSErrorPointer, byAccessor reader: (URL) -> Void)
+    
+    func coordinate(readingDataAt url: URL) async throws -> Data {
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Data, Error>) in
+            var nsError: NSError?
+            self.coordinate(
+                readingItemAt: url, options: .withoutChanges, error: &nsError,
+                byAccessor: { (newURL: URL) -> Void in
+                    do {
+                        let data = try Data(contentsOf: newURL)
+                        continuation.resume(returning: data)
+                        return
+                    } catch {
+                        
+                        continuation.resume(throwing: error)
+                        return
+                    }
+                }
+            )
+            if let nsError = nsError {
+                
+                continuation.resume(throwing: nsError)
+                return
+            }
+        }
+    }
+    */
+    
+}
+
--- Tests/AsyncFileCoordinatorTests/AsyncFileCoordinatorTests.swift
+++ Tests/AsyncFileCoordinatorTests/AsyncFileCoordinatorTests.swift
@@ -2,11 +2,72 @@ import XCTest
 @testable import AsyncFileCoordinator
 
 final class AsyncFileCoordinatorTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    class NSFileCoordinatorStub: NSFileCoordinator {
+        override func coordinate(writingItemAt url: URL, options: NSFileCoordinator.WritingOptions = [], error outError: NSErrorPointer, byAccessor writer: (URL) -> Void) {
+            let error = NSError(domain: NSCocoaErrorDomain, code: NSFileWriteOutOfSpaceError, userInfo: nil)
+            outError?.pointee = error
+        }
     }
+    
+    func testWrite() async throws {
+        let data = Data("testData".utf8)
+        let fileName = "hello"
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let url = documentDirectory.appendingPathComponent(fileName)
+        
+        try await NSFileCoordinator().coordinate(writing: data, at: url)
+    }
+    
+    func testWriteError() async throws {
+        let data = Data("testData".utf8)
+        let fileName = "hello"
+        let urlWeDoNotHavePermissionToWriteTo = URL(fileURLWithPath: "/dev/null")
+        
+        let url = urlWeDoNotHavePermissionToWriteTo.appendingPathComponent(fileName)
+        
+        
+        var error: Error?
+        
+        do {
+            try await NSFileCoordinator().coordinate(writing: data, at: url)
+        } catch let err {
+            error = err
+        }
+        
+        XCTAssertNotNil(error)
+    }
+    
+    func testWriteSimulatedError() async throws {
+        let data = Data("testData".utf8)
+        let fileName = "hello"
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let url = documentDirectory.appendingPathComponent(fileName)
+        
+        var error: Error?
+        
+        do {
+            try await NSFileCoordinatorStub().coordinate(writing: data, at: url)
+        } catch let err {
+            error = err
+        }
+        
+        XCTAssertNotNil(error)
+    }
+    
+    
+    
+   
+    
+    
+//    func testDelete() async throws {
+//        let fileName = "file"
+//        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//        let url = documentDirectory.appendingPathComponent(fileName)
+//        
+//        try await NSFileCoordinator().coordinate(deletingItemAt: url)
+//        
+//    }
+    
+    
 }
